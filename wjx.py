92824e1d37b722266f96d0d6d671c104fa268290
Zhang Hua <zhanghua.00@qq.com>
2020-09-06 18:23:48 +0800

    较多份数时分段处理防止触发验证，加入跳过验证功能的空函数

--- wjx.py
+++ wjx.py
@@ -10,15 +10,26 @@ import shutil
 import zipfile
 import hashlib
 import subprocess
-import string
 import logging
 debug=False
-warn_num=350
+warn_num=375
 if debug==True:
     log_level=logging.DEBUG
 else:
     log_level=logging.INFO
 os.chdir(os.path.split(os.path.realpath(__file__))[0])
+if os.path.exists("log"):
+    shutil.rmtree("log")
+os.mkdir("log")
+logger=logging.getLogger()
+logger.setLevel(log_level)
+formatter=logging.Formatter(fmt="%(asctime)s - %(levelname)s - %(message)s",datefmt="%Y-%m-%d %H:%M:%S")
+console=logging.StreamHandler()
+console.setFormatter(formatter)
+files_=logging.FileHandler(filename="log/main.log",mode="w",encoding="utf-8")
+files_.setFormatter(formatter)
+logger.addHandler(console)
+logger.addHandler(files_)
 def gen_bootstrap():
     lines=[
         "@echo off\n",
@@ -28,25 +39,26 @@ def gen_bootstrap():
         "\""+sys.executable+"\" -m pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple\n",
         "reg ADD HKLM\SOFTWARE\Policies\Google\Chrome /v RendererCodeIntegrityEnabled /t REG_DWORD /d 0 /f\n",
         "del /F /S /Q requirements.txt\n"]
-    with open("bootstrap.bat","w",encoding="utf-8") as gen:
-        gen.writelines(lines)
+    with open("bootstrap.bat","w",encoding="utf-8") as gennerator:
+        generator.writelines(lines)
 try:
     import requests
     from selenium import webdriver
+    from selenium.common.exceptions import NoSuchElementException
     from selenium.webdriver.support.ui import WebDriverWait
     from selenium.webdriver.common.by import By
     from selenium.webdriver.support import expected_conditions
 except:
-    logging.info("正在初始化依赖环境。。。")
+    logger.info("正在初始化依赖环境。。。")
     gen_bootstrap()
     re=subprocess.run("bootstrap.bat")
     if re.returncode==0:
         os.remove("bootstrap.bat")
-        logging.info("初始化依赖环境完成")
+        logger.info("初始化依赖环境完成")
     else:
         raise RuntimeError("初始化依赖环境失败，请手动执行 bootstrap.bat 完成初始化。弹出的UAC认证提示请予以通过")
 else:
-    logging.info("依赖环境正常")
+    logger.info("依赖环境正常")
     if os.path.exists("bootstrap.bat")==True:
         os.remove("bootstrap.bat")
     if os.path.exists("Chrome/env.zip")==True:
@@ -54,7 +66,7 @@ else:
 if os.path.exists("Chrome")==False:
     os.mkdir("Chrome")
 if os.path.exists("Chrome/App/chrome.exe")==False:
-    logging.info("正在初始化运行环境。。。")
+    logger.info("正在初始化运行环境。。。")
     envaddr_list=["https://github.wuyanzheshui.workers.dev","https://download.fastgit.org"]
     def unpack(md5_:str):
         with open("Chrome/env.zip","rb") as file_reader:
@@ -65,7 +77,7 @@ if os.path.exists("Chrome/App/chrome.exe")==False:
             os.remove("Chrome/env.zip")
             return 0
         else:
-            logging.error("文件MD5不符，终止解压缩")
+            logger.error("文件MD5不符，终止解压缩")
             return 1
     def down_env(addr_head:str):
         if os.path.exists("Chrome/env.zip")==False:
@@ -73,7 +85,7 @@ if os.path.exists("Chrome/App/chrome.exe")==False:
             with open("Chrome/env.zip","wb") as file_downloader:
                 file_downloader.write(requests.get(envaddr).content)
         if unpack("dcf2981ec68a72e206f949066ee8eedd")==1:
-            logging.warning("下载失败，请手动下载 "+envaddr+" 并以 env.zip 的文件名保存到Chrome目录下，之后重启程序")
+            logger.warning("下载失败，请手动下载 "+envaddr+" 并以 env.zip 的文件名保存到Chrome目录下，之后重启程序")
             return 1
         else:
             return 0
@@ -87,37 +99,29 @@ if os.path.exists("Chrome/App/chrome.exe")==False:
     choosen_head=random.sample(check_stat(envaddr_list),1)[0]
     if down_env(choosen_head)==1:
         raise RuntimeError("下载运行环境出错，请检查网络连接后重试")
-if os.path.exists("log"):
-    shutil.rmtree("log")
-os.mkdir("log")
-logging.basicConfig(filename="log/main.log",filemode="w",format="%(asctime)s - %(levelname)s - %(message)s",datefmt="%Y-%m-%d %H:%M:%S",level=log_level)
-logger=logging.getLogger()
-formatter=logging.Formatter(fmt="%(asctime)s - %(levelname)s - %(message)s",datefmt="%Y-%m-%d %H:%M:%S")
-console=logging.StreamHandler()
-console.setLevel(log_level)
-console.setFormatter(formatter)
-logger.addHandler(console)
 times=int(input("请输入生成的问卷的份数："))
 if times>=warn_num:
-    logging.warning("当前问卷份数较多，大于 %s 次，较易触发unknown error错误。如果触发，请重新执行脚本。" %warn_num)
+    logger.warning("当前问卷份数较多，大于 %s 次，较易触发unknown error错误。如果触发，请重新执行脚本。" %warn_num)
 print("问卷星地址举例：https://www.wjx.cn/jq/89714348.aspx")
 url=str(input("请输入问卷星创建的问卷地址："))
 url="https://www.wjx.cn/jq/"+url.split("/")[-1]
-logging.info("转换地址完成，为："+url)
+logger.info("转换地址完成，为："+url)
 start_time=time.time()
-def do_survey(url:str,logger:logging.Logger):
+def do_survey(url_2:str,logger_:logging.Logger):
     browser=webdriver.ChromeOptions()
     browser.binary_location="./Chrome/App/chrome.exe"
     if debug==False:
         browser.add_argument("headless")
-    driver = webdriver.Chrome("./Chrome/app/chromedriver.exe",options=browser)
-    driver.get(url)
+    browser.add_argument("user-agent=\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36\"")
+    driver = webdriver.Chrome("./Chrome/app/chromedriver.exe",options=browser,service_log_path="log/driver.log")
+    driver.get(url_2)
     wait = WebDriverWait(driver, 10)
     element = wait.until(expected_conditions.element_to_be_clickable((By.ID,'submit_button')))
     def do_queue(driver_=driver):
         root_element=driver_.find_element_by_xpath("/html/body/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]/fieldset")
         question_elements=root_element.find_elements_by_class_name("div_question")
         def gen_str(num:int):
+            import string
             return ''.join(random.sample(string.ascii_letters + string.digits, num))
         for element in question_elements:
             element_pos=question_elements.index(element)
@@ -158,11 +162,25 @@ def do_survey(url:str,logger:logging.Logger):
                 choose_answer_title=text+"\n"
             else:
                 raise RuntimeError("无法获取正确的元素，请重试！")
-            logger.info("问题："+question_title+"\n选择："+choose_answer_title+"\n")
+            logger_.info("问题："+question_title+"\n选择："+choose_answer_title+"\n")
     do_queue(driver_=driver)
+    time.sleep(random.randint(1,5))
     driver.find_element_by_xpath("/html/body/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[4]/table/tbody/tr/td/input").click()
-    logger.info("已提交记录")
-    driver.quit()
+    try:
+        target=driver.find_element_by_xpath("/html/body/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[1]/div[1]")
+    except NoSuchElementException:
+        logger_.info("未找到验证元素，看起来没有触发验证")
+        logger_.info("已提交记录")
+        driver.quit()
+        return True
+    else:
+        logger_.warning("找到验证元素，似乎已经触发验证，已记录失败次数")
+        if target.get_attribute("id")=="SM_BTN_1":
+            def bypass_captcha(driver_,element_):
+                pass
+            bypass_captcha(driver_=driver,element_=target)
+        driver.quit()
+        return False
 class job_thread(threading.Thread):
     def __init__(self,id_:int,times:int,url:str):
         self.id=id_
@@ -173,12 +191,39 @@ class job_thread(threading.Thread):
         self.thread_logger.addHandler(thread_log_handler)
         threading.Thread.__init__(self)
     def run(self):
+        self.failed_num=0
         self.thread_logger.info("线程 "+str(self.id)+" 开始执行")
+        max_conn=3
         for each_time in range(self.times):
-            do_survey(self.url,self.thread_logger)
+            def finish_survey(url_:str,each_time_:int,logger_2=self.thread_logger):
+                if do_survey(url_2=url_,logger_=logger_2)==False:
+                    self.failed_num=self.failed_num+1
+                    logger_2.warning("线程 %s 在第 %d 次执行触发验证，提交失败" %(self.id,each_time_))
+                else:
+                    logger_2.info("线程 %d 在第 %d 次执行成功提交数据" %(self.id,each_time_))
+            if self.times>0 and self.times<=max_conn:
+                for each_time in range(self.times):
+                    finish_survey(url_=self.url,each_time_=each_time)
+            else:
+                self.thread_logger.info("执行次数较多，将分成每 %d 次一组，每组间隔一定时间完成以避免验证" %max_conn)
+                times_,more_times_=divmod(self.times,max_conn)
+                if more_times_!=0:
+                    for time_ in range(more_times_):
+                        finish_survey(url_=self.url,each_time_=time_)
+                    self.thread_logger.info("线程 %s 第 %d 次执行完成" %(self.id,time_))
+                    time.sleep(random.randint(3,5))
+                for time_ in range(times_):
+                    for each_conn in range(max_conn):
+                        finish_survey(url_=self.url,each_time_=each_conn)
+                    self.thread_logger.info("线程 %s 第 %d 次执行完成" %(self.id,each_conn))
+                    time.sleep(random.randint(3,5))
+            time.sleep(random.randint(5,7))
+
+        logger.info("线程 %d 结束运行" %self.id)
 threads=[]
 thread_num=int(cpu_count()/2)
 full_times,more_time=divmod(times,thread_num)
+all_faileds=0
 for cpu in range(thread_num):
     working_thread=job_thread(cpu,full_times,url)
     threads.append(working_thread)
@@ -188,6 +233,7 @@ threads.append(more_thread)
 more_thread.start()
 for thread in threads:
     thread.join()
+    all_faileds=all_faileds+thread.failed_num
 m,s=divmod(int(time.time()-start_time),60)
 h,m=divmod(m,60)
-logging.info("执行完成，选择内容可查看日志文件输出记录,用时 %02d:%02d:%02d 部分问卷可能由于网站防护机制未能提交，请手动登陆网页后台查看。" %(h, m, s))
+logger.info("执行完成，选择内容可查看日志文件输出记录，用时 %02d:%02d:%02d 其中 %d 份问卷由于网站防护机制未能提交。" %(h, m, s, all_faileds))

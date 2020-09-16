dbb171a79dc52556e776ecdb3626614b9f69a04b
Zhang Hua <zhanghua.00@qq.com>
2020-09-16 18:50:30 +0800

    修正日志记录问题

--- wjx.py
+++ wjx.py
@@ -1,109 +1,140 @@
-from multiprocessing import cpu_count, Process
-def do_survey(url_2:str,logger_,debug:bool):
-    import time
-    import random
-    from selenium import webdriver
-    from selenium.common.exceptions import NoSuchElementException, WebDriverException
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.support import expected_conditions
-    browser=webdriver.ChromeOptions()
-    browser.binary_location="./Chrome/App/chrome.exe"
-    if debug==False:
-        browser.add_argument("headless")
-    try:
-        driver = webdriver.Chrome("./Chrome/app/chromedriver.exe",options=browser)
-    except WebDriverException:
-        logger_.error("触发unknown error错误")
-        return False
-    driver.get(url_2)
-    element = WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable((By.ID,'submit_button')))
-    def do_queue(driver_=driver,logger_=logger_):
-        question_elements=driver_.find_elements_by_xpath('//div[@class="div_question"]')
-        def gen_str(num:int):
-            import string
-            return ''.join(random.sample(string.ascii_letters + string.digits, num))    
-        for question_element in question_elements:
-            question_element_pos=question_elements.index(question_element)
-            question_title=question_element.find_element_by_class_name("div_title_question").text
-            question_answers=question_element.find_elements_by_tag_name("li")
-            removed_num=0
-            for item in question_answers:
-                if item.get_attribute("class")=="notchoice":
-                    question_answers.remove(item)
-            logger_.info("已清理无效元素共 %d 个" %removed_num)
-            sample_element=question_answers[0].find_element_by_tag_name("input")
-            if sample_element.get_attribute("type")=="radio":
-                targets=random.sample(question_answers,1)
-                choose_answer_title=targets[0].find_element_by_tag_name("label").text
-                targets[0].find_element_by_tag_name("a").click()
-                time.sleep(random.randint(1,3))
-            elif sample_element.get_attribute("type")=="checkbox":
-                choose_num=random.randint(2,len(question_answers))
-                choose_answers=[]
-                choose_answers_pos=[]
-                choose_answer_title=""
-                targets=random.sample(question_answers,choose_num)
-                for target in targets:
-                    choose_answers.append(target)
-                    choose_answers_pos.append(question_answers.index(target))
-                for answer in choose_answers:
-                    text=""
-                    answer.find_element_by_tag_name("a").click()
-                    choose_answer_title=answer.find_element_by_tag_name("label").text
-                    if len(answer.find_elements_by_tag_name("input"))==2:
-                        text_input=answer.find_elements_by_tag_name("input")[1]
-                        text_input.click()
-                        text_input.clear()
-                        text=gen_str(random.randint(5,10))
-                        text_input.send_keys(text)
-                        choose_answer_title=choose_answer_title+text+"\n"
-                    else:
-                        choose_answer_title=choose_answer_title+"\n"
-                    time.sleep(random.randint(1,3))
-            elif sample_element.get_attribute("type")=="text":
-                target=question_answers[0]
-                target.click()
-                target.clear()
-                text=gen_str(random.randint(10,20))
-                target.send_keys(text)
-                choose_answer_title=text+"\n"
-            else:
-                raise RuntimeError("无法获取正确的元素，请重试！")
-            logger_.info("选择内容：\n问题："+question_title+"\n选择："+choose_answer_title+"\n")
-    do_queue(driver_=driver)
-    time.sleep(random.randint(1,3))
-    driver.find_element_by_tag_name("td").find_element_by_tag_name("input").click()
-    try:
-        target=driver.find_element_by_xpath("/html/body/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[1]/div[1]")
-    except NoSuchElementException:
-        logger_.info("未找到验证元素，看起来没有触发验证")
-        logger_.info("已提交记录")
-        driver.quit()
-        return True
+from multiprocessing import cpu_count, Process, Manager, Queue
+class config:
+    debug=False
+    warn_num=375
+def process_log(queue):
+    import logging
+    logger_=logging.getLogger()
+    if config.debug==True:
+        log_level=logging.DEBUG
     else:
-        logger_.warning("找到验证元素，似乎已经触发验证，已记录失败次数")
-        if target.get_attribute("id")=="SM_BTN_1":
-            def bypass_captcha(driver_,element_):
-                return False
-            status=bypass_captcha(driver_=driver,element_=target)
-        driver.quit()
-        return False
-def multicoreproc(id_:int,url_:str,times:int,log_level:int,debug:bool):
+        log_level=logging.INFO
+    logger_.setLevel(log_level)
+    formatter=logging.Formatter(fmt="%(asctime)s - %(levelname)s - %(message)s",datefmt="%Y-%m-%d %H:%M:%S")
+    handler=logging.FileHandler(filename="log/thread.log",mode="w",encoding="utf-8",delay=True)
+    handler.setFormatter(formatter)
+    logger_.addHandler(handler)
+    while True:
+        record=queue.get()
+        if record==None:
+            break
+        logger=logging.getLogger(record.name)
+        logger.handle(record)
+def multicoreproc(id_:int,url_:str,times:int,queue):
     import logging
     import random
     import time
+    import logging.handlers
     max_conn=3
     failed_num=0
-    thread_logger=logging.getLogger("thread_logger_"+str(id_))
-    files_handler=logging.FileHandler(filename="log/"+str(id_)+".log",mode="w",encoding="utf-8",delay=True)
-    files_handler.setFormatter(logging.Formatter(fmt="%(asctime)s - %(levelname)s - %(message)s",datefmt="%Y-%m-%d %H:%M:%S"))
-    files_handler.setLevel(log_level)
-    thread_logger.addHandler(files_handler)
+    thread_logger=logging.getLogger()
+    handler=logging.handlers.QueueHandler(queue)
+    thread_logger.addHandler(handler)
+    if config.debug==True:
+        thread_logger.setLevel(logging.DEBUG)
+    else:
+        thread_logger.setLevel(logging.INFO)
+    thread_logger.info("开始执行线程 %d 的工作内容" %id_)
+    def do_survey(url_2:str,thread_id:int):
+        import time
+        import random
+        from selenium import webdriver
+        from selenium.common.exceptions import NoSuchElementException, WebDriverException
+        from selenium.webdriver.support.ui import WebDriverWait
+        from selenium.webdriver.common.by import By
+        from selenium.webdriver.support import expected_conditions
+        browser=webdriver.ChromeOptions()
+        browser.binary_location="./Chrome/App/chrome.exe"
+        if config.debug==False:
+            browser.add_argument("headless")
+        try:
+            driver = webdriver.Chrome(executable_path="./Chrome/app/chromedriver.exe",options=browser)
+        except WebDriverException:
+            thread_logger.error("线程 %d 触发unknown error错误" %thread_id)
+            return False
+        driver.get(url_2)
+        element = WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable((By.ID,'submit_button')))
+        def do_queue(driver_=driver):
+            question_elements=driver_.find_elements_by_xpath('//div[@class="div_question"]')
+            def gen_str(num:int):
+                import string
+                return ''.join(random.sample(string.ascii_letters + string.digits, num))    
+            for question_element in question_elements:
+                question_element_pos=question_elements.index(question_element)
+                question_title=question_element.find_element_by_class_name("div_title_question").text
+                question_answers=question_element.find_elements_by_tag_name("li")
+                if len(question_answers)==0:
+                    question_answers=[question_element.find_element_by_tag_name("textarea")]
+                removed_num=0
+                for item in question_answers:
+                    if item.get_attribute("class")=="notchoice":
+                        question_answers.remove(item)
+                        removed_num=removed_num+1
+                thread_logger.info("线程 %d 已清理无效元素共 %d 个" %(thread_id,removed_num))
+                try:
+                    sample_element=question_answers[0].find_element_by_tag_name("input")
+                except NoSuchElementException:
+                    sample_element=question_answers[0]
+                if sample_element.get_attribute("type")=="radio":
+                    targets=random.sample(question_answers,1)
+                    choose_answer_title=targets[0].find_element_by_tag_name("label").text
+                    targets[0].find_element_by_tag_name("a").click()
+                    time.sleep(random.randint(1,3))
+                elif sample_element.get_attribute("type")=="checkbox":
+                    choose_num=random.randint(2,len(question_answers))
+                    choose_answers=[]
+                    choose_answers_pos=[]
+                    choose_answer_title=""
+                    targets=random.sample(question_answers,choose_num)
+                    for target in targets:
+                        choose_answers.append(target)
+                        choose_answers_pos.append(question_answers.index(target))
+                    for answer in choose_answers:
+                        text=""
+                        choose_answer_title=""
+                        answer.find_element_by_tag_name("a").click()
+                        choose_answer_title=choose_answer_title+answer.find_element_by_tag_name("label").text
+                        if len(answer.find_elements_by_tag_name("input"))==2:
+                            text_input=answer.find_elements_by_tag_name("input")[1]
+                            text_input.click()
+                            text_input.clear()
+                            text=gen_str(random.randint(5,10))
+                            text_input.send_keys(text)
+                            choose_answer_title=choose_answer_title+text+"\n"
+                        else:
+                            choose_answer_title=choose_answer_title+"\n"
+                        time.sleep(random.randint(1,3))
+                elif sample_element.get_attribute("class")=="inputtext":
+                    target=question_answers[0]
+                    target.click()
+                    target.clear()
+                    text=gen_str(random.randint(10,20))
+                    target.send_keys(text)
+                    choose_answer_title=text+"\n"
+                else:
+                    raise RuntimeError("无法获取正确的元素，请重试！")
+                thread_logger.info("线程 "+str(thread_id)+" 选择内容：\n问题："+question_title+"\n选择："+choose_answer_title+"\n")
+        do_queue(driver_=driver)
+        time.sleep(random.randint(1,3))
+        driver.find_element_by_tag_name("td").find_element_by_tag_name("input").click()
+        try:
+            target=driver.find_element_by_xpath('//span[@id="SM_TXT_1"]')
+        except NoSuchElementException:
+            thread_logger.info("线程 %d 未找到验证元素，看起来没有触发验证" %thread_id)
+            driver.quit()
+            return True
+        else:
+            thread_logger.warning("线程 %d 找到验证元素，似乎已经触发验证，已记录失败次数" %thread_id)
+            if target.get_attribute("id")=="SM_BTN_1":
+                def bypass_captcha(driver_,element_):
+                    pass
+                bypass_captcha(driver_=driver,element_=target)
+            driver.quit()
+            return False
     if times>0:
         if times<=max_conn:
             for time0 in range(times):
-                if do_survey(url_2=url_,logger_=thread_logger,debug=debug)==False:
+                if do_survey(url_2=url_,thread_id=id_)==False:
                     failed_num=failed_num+1
                     thread_logger.error("线程 %d 第 %d 次提交失败" %(id_,time0))
                 else:
@@ -112,16 +143,17 @@ def multicoreproc(id_:int,url_:str,times:int,log_level:int,debug:bool):
         else:
             times1,more_times=divmod(times,max_conn)
             if more_times!=0:
+                group=-1
                 for time1 in range(more_times):
-                    if do_survey(url_2=url_,logger_=thread_logger,debug=debug)==False:
+                    if do_survey(url_2=url_,thread_id=id_)==False:
                         failed_num=failed_num+1
-                        thread_logger.error("线程 %d 第 %d 次提交失败" %(id_,time1))
+                        thread_logger.error("线程 %d 第 %d 组第 %d 次提交失败" %(id_,group,time1))
                     else:
-                        thread_logger.info("线程 %d 第 %d 次提交成功" %(id_,time1))
+                        thread_logger.info("线程 %d 第 %d 组第 %d 次提交成功" %(id_,group,time1))
                     time.sleep(random.randint(1,3))
-            for time2 in range(times1):
+            for time2 in range(more_times,times1+more_times):
                 for conn in range(max_conn):
-                    if do_survey(url_2=url_,logger_=thread_logger,debug=debug)==False:
+                    if do_survey(url_2=url_,thread_id=id_)==False:
                         failed_num=failed_num+1
                         thread_logger.error("线程 %d 第 %d 组第 %d 次提交失败" %(id_,time2,conn))
                     else:
@@ -135,7 +167,7 @@ def multicoreproc(id_:int,url_:str,times:int,log_level:int,debug:bool):
         thread_logger.warning("线程 %d 失败 %d 次，等待 %2d 秒后将继续补齐失败次数" %(id_,failed_num,pause_time))
         times=failed_num
         time.sleep(pause_time)
-        multicoreproc(id_=id_,url_=url_,times=times,log_level=log_level,debug=debug)
+        multicoreproc(id_=id_,url_=url_,times=times,queue=queue)
     thread_logger.info("线程 %d 结束运行" %id_)
 if __name__=="__main__":
     import os 
@@ -144,11 +176,9 @@ if __name__=="__main__":
     import logging
     import subprocess
     import time
-    debug=False
-    warn_num=375
     if os.name!="nt":
         raise RuntimeError("此脚本仅支持Windows XP SP2及更高版本")
-    if debug==True:
+    if config.debug==True:
         log_level=logging.DEBUG
     else:
         log_level=logging.INFO
@@ -156,12 +186,13 @@ if __name__=="__main__":
     if os.path.exists("log")==True:
         shutil.rmtree("log")
     os.mkdir("log")
+    queue=Queue(-1)
     logger=logging.getLogger()
     logger.setLevel(log_level)
     formatter=logging.Formatter(fmt="%(asctime)s - %(levelname)s - %(message)s",datefmt="%Y-%m-%d %H:%M:%S")
     console=logging.StreamHandler()
     console.setFormatter(formatter)
-    files_=logging.FileHandler(filename="log/main.log",mode="w",encoding="utf-8")
+    files_=logging.FileHandler(filename="log/main.log",mode="w",encoding="utf-8",delay=True)
     files_.setFormatter(formatter)
     logger.addHandler(console)
     logger.addHandler(files_)
@@ -303,8 +334,8 @@ if __name__=="__main__":
     else:
         raise ValueError("意料之外的返回值")
     times=int(input("请输入生成的问卷的份数："))
-    if times>=warn_num:
-        logger.warning("当前问卷份数较多，大于 %s 次，较易出现验证。" %warn_num)
+    if times>=config.warn_num:
+        logger.warning("当前问卷份数较多，大于 %s 次，较易出现验证。" %config.warn_num)
     print("问卷星地址举例：https://www.wjx.cn/jq/89714348.aspx")
     url=str(input("请输入问卷星创建的问卷地址："))
     url="https://www.wjx.cn/jq/"+url.split("/")[-1].replace(" ","")
@@ -313,18 +344,22 @@ if __name__=="__main__":
     threads=[]
     thread_num=int(cpu_count()/2)
     times_,more_times_=divmod(times,thread_num)
+    logger_process=Process(target=process_log,args=(queue,))
+    logger_process.start()
     if more_times_!=0:
-        more_thread=Process(target=multicoreproc,args=(thread_num+1,url,more_times_,log_level,debug))
+        more_thread=Process(target=multicoreproc,args=(thread_num+1,url,more_times_,queue))
         more_thread.start()
         threads.append(more_thread)
     if times_!=0:
         for thread_id in range(thread_num):
-            thread=Process(target=multicoreproc,args=(thread_id,url,times_,log_level,debug))
+            thread=Process(target=multicoreproc,args=(thread_id,url,times_,queue))
             thread.start()
             threads.append(thread)
     if len(threads)!=0:
         for thread_ in threads:
             thread_.join()
+    queue.put_nowait(None)
+    logger_process.join()
     m,s=divmod(int(time.time()-start_time),60)
     h,m=divmod(m,60)
     logger.info("执行完成，选择内容可查看日志文件输出记录，用时 %02d:%02d:%02d 共提交 %d 份问卷。" %(h, m, s, times))
